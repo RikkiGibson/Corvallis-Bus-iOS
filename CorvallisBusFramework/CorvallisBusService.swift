fc6550ab7d744fbd8dfe6a0976d601a409c108c9
Rikki Gibson <rikkigibson@gmail.com>
2014-10-25 21:13:53 -0700

    removed derpy print statement

--- CorvallisBusFramework/CorvallisBusService.swift
+++ CorvallisBusFramework/CorvallisBusService.swift
@@ -45,9 +45,6 @@ struct CorvallisBusService {
                     }
                     
                     self._stops = stopJson.mapUnwrap() { BusStop(data: $0) }
-                    for stop in self._stops! {
-                        println(stop.name)
-                    }
                     callback(self._stops!)
             }).resume()
         }
