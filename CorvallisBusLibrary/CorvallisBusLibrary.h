3cac0378ed0d8fc771b0ee59e42a9867762e8a23
Rikki Gibson <rikkigibson@gmail.com>
2014-11-04 20:28:31 -0800

    CorvallisBusFramework devolved into CorvallisBusLibrary! (necessary for iOS 7 support)

---
+++ CorvallisBusLibrary/CorvallisBusLibrary.h
@@ -0,0 +1,13 @@
+//
+//  CorvallisBusLibrary.h
+//  CorvallisBusLibrary
+//
+//  Created by Rikki Gibson on 11/4/14.
+//  Copyright (c) 2014 Rikki Gibson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CorvallisBusLibrary : NSObject
+
+@end
---
+++ CorvallisBusLibrary/CorvallisBusLibrary.m
@@ -0,0 +1,13 @@
+//
+//  CorvallisBusLibrary.m
+//  CorvallisBusLibrary
+//
+//  Created by Rikki Gibson on 11/4/14.
+//  Copyright (c) 2014 Rikki Gibson. All rights reserved.
+//
+
+#import "CorvallisBusLibrary.h"
+
+@implementation CorvallisBusLibrary
+
+@end
