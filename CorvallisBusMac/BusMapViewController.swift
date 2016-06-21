598fbd16bd285b34fb0a94c4bf511d4edc2564b3
Rikki Gibson <rikkigibson@gmail.com>
2016-06-21 13:21:58 -0700

    Remove favorites table from main app

--- CorvallisBusMac/BusMapViewController.swift
+++ CorvallisBusMac/BusMapViewController.swift
@@ -13,15 +13,28 @@ protocol BusMapViewControllerDataSource : class {
     func busStopAnnotations() -> Promise<[Int : BusStopAnnotation], BusError>
 }
 
+let CORVALLIS_LOCATION = CLLocation(latitude: 44.56802, longitude: -123.27926)
+let DEFAULT_SPAN = MKCoordinateSpanMake(0.01, 0.01)
 class BusMapViewController: NSViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     weak var dataSource: BusMapViewControllerDataSource?
+    let locationManagerDelegate = PromiseLocationManagerDelegate()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
-        // Do view setup here.
+        mapView.showsUserLocation = true
+        mapView.setRegion(MKCoordinateRegion(center: CORVALLIS_LOCATION.coordinate, span: DEFAULT_SPAN), animated: false)
+        locationManagerDelegate.userLocation { maybeLocation in
+            // Don't muck with the location if an annotation is selected right now
+            guard self.mapView.selectedAnnotations.isEmpty else { return }
+            // Only go to the user's location if they're within about 20 miles of Corvallis
+            if case .Success(let location) = maybeLocation where location.distanceFromLocation(CORVALLIS_LOCATION) < 32000 {
+                let region = MKCoordinateRegion(center: location.coordinate, span: DEFAULT_SPAN)
+                self.mapView.setRegion(region, animated: false)
+            }
+        }
         
         dataSource?.busStopAnnotations().startOnMainThread(onStopsLoaded)
     }
@@ -38,14 +51,14 @@ class BusMapViewController: NSViewController, MKMapViewDelegate {
     
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
         if let annotation = annotation as? BusStopAnnotation {
-            let annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(String(String)) ??
-                MKAnnotationView(annotation: annotation, reuseIdentifier: String(String))
+            let annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(String(BusStopAnnotation)) ??
+                MKAnnotationView(annotation: annotation, reuseIdentifier: String(BusStopAnnotation))
             annotationView.updateWithBusStopAnnotation(annotation, isSelected: false)
             return annotationView
         }
         
-        return mapView.dequeueReusableAnnotationViewWithIdentifier("MKAnnotationView") ??
-               MKAnnotationView(annotation: annotation, reuseIdentifier: "MKAnnotationView")
+        return mapView.dequeueReusableAnnotationViewWithIdentifier(String(MKAnnotationView)) ??
+               MKAnnotationView(annotation: annotation, reuseIdentifier: String(MKAnnotationView))
     }
     
     func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
