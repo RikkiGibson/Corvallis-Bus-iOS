af5926b41c0c425a1a9de16469c18151e93553da
Christian Mello <ecksian@icloud.com>
2017-03-01 22:32:06 -0800

    Added table row and view controllers

--- CorvallisBusWatch Extension/FavoritesInterfaceController.swift
+++ CorvallisBusWatch Extension/FavoritesInterfaceController.swift
@@ -1,6 +1,6 @@
 //
-//  InterfaceController.swift
-//  CorvallisBusWatch Extension
+//  FavoritesInterfaceController.swift
+//  CorvallisBus
 //
 //  Created by Christian Mello on 3/1/17.
 //  Copyright © 2017 Rikki Gibson. All rights reserved.
@@ -10,19 +10,22 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class FavoritesInterfaceController: WKInterfaceController {
 
+    @IBOutlet var favoritesTable: WKInterfaceTable!
+    
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+        favoritesTable.setNumberOfRows(10, withRowType: "FavoritesRow")
         // Configure interface objects here.
     }
-    
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-    
+
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
