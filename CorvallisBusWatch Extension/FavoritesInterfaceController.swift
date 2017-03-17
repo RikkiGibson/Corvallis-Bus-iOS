f6471753d5ee33ac24bd8a4371404522bd126584
Christian Mello <ecksian@icloud.com>
2017-03-17 02:57:14 -0700

    Added refresh placeholder.

--- CorvallisBusWatch Extension/FavoritesInterfaceController.swift
+++ CorvallisBusWatch Extension/FavoritesInterfaceController.swift
@@ -13,6 +13,16 @@ import Foundation
 class FavoritesInterfaceController: WKInterfaceController {
 
     @IBOutlet var favoritesTable: WKInterfaceTable!
+    @IBAction func btnRefresh() {
+        print("Refreshing")
+        let cachedFavorites = CorvallisBusFavoritesManager.cachedFavoriteStopsForWidget()
+        favoritesTable.setNumberOfRows(cachedFavorites.count, withRowType: "FavoritesRow")
+        
+        for i in 0..<favoritesTable.numberOfRows {
+            guard let controller = favoritesTable.rowController(at: i) as? FavoritesRowController else { continue }
+            controller.update(with: cachedFavorites[i])
+        }
+    }
     
     
     override func awake(withContext context: Any?) {
@@ -28,6 +38,24 @@ class FavoritesInterfaceController: WKInterfaceController {
         CorvallisBusFavoritesManager.favoriteStopsForWidget()
             .startOnMainThread(onStopsLoaded)
     }
+    
+//    func reloadCachedFavorites() {
+//        let viewModels = CorvallisBusFavoritesManager.cachedFavoriteStopsForWidget()
+//        favoriteStops = .success(viewModels)
+//        if #available(iOSApplicationExtension 10.0, *) {
+//            extensionContext?.widgetLargestAvailableDisplayMode = viewModels.count > 1 ? .expanded : .compact
+//        }
+//        tableView.reloadData()
+//    }
+//    
+//    func watchPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+//        reloadCachedFavorites()
+//        
+//        didCompleteUpdate = false
+//        CorvallisBusFavoritesManager.favoriteStopsForWidget()
+//            .startOnMainThread({ self.onUpdate($0, completionHandler: completionHandler) })
+//        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(TodayViewController.clearTableIfUpdatePending), userInfo: nil, repeats: false)
+//    }
 
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
