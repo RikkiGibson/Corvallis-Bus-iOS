26348d33cd1d9c0edd4ceef959a82652c06e917c
Rikki Gibson <rikkigibson@gmail.com>
2017-03-29 20:28:00 -0700

    Fix crash when selecting table row and when reloading data. Add simple row tapped action.

--- CorvallisBusWatch Extension/FavoritesRowController.swift
+++ CorvallisBusWatch Extension/FavoritesRowController.swift
@@ -7,10 +7,8 @@
 //
 
 import WatchKit
-import Foundation
 
-
-class FavoritesRowController: WKInterfaceController {
+class FavoritesRowController: NSObject {
 
     @IBOutlet var lblStopName: WKInterfaceLabel!
     
@@ -24,22 +22,6 @@ class FavoritesRowController: WKInterfaceController {
         print("Refreshing!")
     }
     
-    override func awake(withContext context: Any?) {
-        super.awake(withContext: context)
-        
-        // Configure interface objects here.
-    }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
     func update(with model: FavoriteStopViewModel) {
         lblStopName.setText(model.stopName)
         
